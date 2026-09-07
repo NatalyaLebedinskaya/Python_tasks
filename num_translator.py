1b02b0101fb6fde379d575fdda6e8d8527a086d0
NatalyaLebedinskaya <onehundredsevensun@gmail.com>
2026-09-07 19:26:08 +0700

    Update num_translator.py

--- num_translator.py
+++ num_translator.py
@@ -1,25 +1,16 @@
 def get_num():
-    while True:
+    num = 0
+    while num < 1 or num > 5:
         try:
             num = int(input("Введите целое число от 1 до 5: "))
-            if 1 <= num <= 5:
-                return num
-            else:
-                print("Ошибка: число должно быть в диапазоне от 1 до 5")
         except ValueError:
             print("Ошибка: нужно ввести целое число")
+    return num
 
 def translate_num(num):
-    if num == 1:
-        return "One"
-    elif num == 2:
-        return "Two"
-    elif num == 3:
-        return "Three"
-    elif num == 4:
-        return "Four"
-    elif num == 5:
-        return "Five"
+    translator = {1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five"}
+    if num in translator:
+        return translator[num]
     else:
         return "Unknown"
 
@@ -27,5 +18,3 @@ def translate_num(num):
 number = get_num()
 translation = translate_num(number)
 print(translation)
-
-
